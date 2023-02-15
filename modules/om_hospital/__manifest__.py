f24e94e78bc354dd2969b3ceccc8da59b22cf7cc
dohungptit98 <hungdoptit063@gmail.com>
2023-02-15 11:02:49 +0700

    additional appointment model

---
+++ modules/om_hospital/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'HungDo',
+    'sequence': -100,
+    'summary': 'Hospital management system',
+    'description': """
+        Hospital management system
+    """,
+    'depends': ['mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+        'views/female_patient_view.xml',
+        'views/appointment_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ modules/om_hospital/models/appointment.py
@@ -0,0 +1,28 @@
+from odoo import api, fields, models
+
+class HospitalAppointment(models.Model):
+    _name = "hospital.appointment"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Hospital Appointment"
+    _rec_name = "ref"
+
+    patient_id = fields.Many2one('hospital.patient', string="Patient")
+    gender = fields.Selection(related='patient_id.gender')
+    appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
+    booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
+    ref = fields.Char(string="Reference")
+    prescription = fields.Html(string="Prescription")
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], string="Priority")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'In Consultation'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')], default='draft', string="Status", required=True)
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
